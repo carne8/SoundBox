3d0ff3fbf6ab00c410d957014c9f63d38262b5af
Gaël Bouquain <gael.bouquain@gmail.com>
2023-02-26 15:34:45 +0100

    Fix text

--- src/Components/UpdateDialog.fs
+++ src/Components/UpdateDialog.fs
@@ -10,7 +10,10 @@ let show context totalOperations onValidated () =
         context = context,
         builder = (fun _ctx -> AlertDialog(
             title = Text("Perform update ?", textAlign = Dart.TextAlign.center),
-            content = Text($"This update will perform {totalOperations} operations", textAlign = Dart.TextAlign.center),
+            content =
+                (match totalOperations with
+                 | x when x = 1 -> Text($"This update will perform {x} operation", textAlign = Dart.TextAlign.center)
+                 | x -> Text($"This update will perform {x} operations", textAlign = Dart.TextAlign.center)),
             actionsAlignment = MainAxisAlignment.spaceBetween,
             actions = [|
                 OutlinedButton(
